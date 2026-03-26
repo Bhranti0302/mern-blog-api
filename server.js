1597afb116fc6c31f56e2d5504d2a0e396f23dfb
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-03-26 21:51:06 +0530

    Setup express server and Add Mongodb connection

---
+++ server.js
@@ -0,0 +1,19 @@
+const app=require("./app");
+require("dotenv").config();
+const connectDB=require("./config/db");
+
+const PORT=process.env.PORT || 5000;
+
+const startServer=async()=>{
+    try{
+        await connectDB();
+
+        app.listen(PORT,()=>{
+            console.log(`Server running on port ${PORT}`);
+        })
+    } catch(error){
+       console.error("Failed to start server:",error.message);
+    }
+}
+
+startServer();
