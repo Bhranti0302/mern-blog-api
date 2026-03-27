28498dac5c49414fba96046ae6d56fbb6db7ccec
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-03-27 22:16:41 +0530

    Modified Post Schema

--- models/Post.js
+++ models/Post.js
@@ -1,48 +1,51 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const postSchema=new mongoose.Schema({
-    title:{
-        type:String,
-        required:[true,"Please enter the title of the post"],
-        minlength:2,
-        maxlength:100,
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please enter the title of the post"],
+      minlength: 2,
+      maxlength: 100,
     },
 
-    content:{
-        type:String,
-        required:[true,"Please enter the content of the post"],
-        minlength:10,
-        maxlength:5000,
+    content: {
+      type: String,
+      required: [true, "Please enter the content of the post"],
+      minlength: 10,
+      maxlength: 5000,
     },
 
-    slug:{
-        type:String,
-        unique:true,
+    slug: {
+      type: String,
+      unique: true,
     },
 
-    coverImage:{
-        type:String,
-        required:[true,"Please enter the cover image of the post"],
+    coverImage: {
+      type: String,
+      required: [true, "Please enter the cover image of the post"],
     },
 
-    isPublished:{
-        type:Boolean,
-        default:false,
+    isPublished: {
+      type: Boolean,
+      default: false,
     },
 
-    tags:{
-        type:[String],
+    tags: {
+      type: [String],
     },
 
-    views:{
-        type:Number,
-        default:0
-    }
-},{
-    timestamps:true,
-})
+    views: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-module.exports=mongoose.model("Post",postSchema);
+module.exports = mongoose.model("Post", postSchema);
 
 // Used for:
 // 1.Create blog
