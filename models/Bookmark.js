f3917e0a2b61dad96b5758f4399606f1d056cd0c
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-03-27 22:30:39 +0530

    Created Bookmark Schema

---
+++ models/Bookmark.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+
+const bookmarkSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },
+
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Post",
+    }
+})
+
+module.exports=mongoose.model("Bookmark",bookmarkSchema);
+
+// Save posts for later
