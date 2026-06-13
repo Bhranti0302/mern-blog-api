0099c07432ed5becbf67e66b0d0186f4f9359f35
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-06-13 22:53:24 +0530

    feat: add user registration logic with validation and JWT generation

---
+++ controllers/authController.js
@@ -0,0 +1,44 @@
+const User = require("../models/User"); 
+const generateToken = require("../utils/generateToken");
+const cookieOptions = require("../utils/cookieOptions");
+const jwt = require("jsonwebtoken");
+
+// ================== Register ===================
+exports.register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        
+        // 1. Check user exists
+        const userExists = await User.findOne({ email });
+        
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // 2. create user
+        const user = await User.create({
+            name,
+            email,
+            password
+        }),
+
+        // 3. Generate JWT
+        const token = generateToken(user._id);
+
+        // 4. Send cookie
+        res.cookie("token", token, cookieOptions)
+
+        // 5. Send response
+        res.status(201).json({
+            message: "User registered successfully",
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
